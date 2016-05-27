26f3fc9cfce34ad8b2a3c5498716870ad1fdee56
Sander van der Burg <svanderburg@mgail.com>
2016-05-27 13:40:03 +0200

    Use a different work around for Object.create. Now we just provide the stream object a dummy prototype

--- overrides.js
+++ overrides.js
@@ -1,2 +1,7 @@
+// Globally override some properties to make the library work
+
+// Fool the XMPP library to believe that it runs in a browser
 process.browser = true;
-Object.create = null;
+
+// Set the prototype to an empty object of the result stream instance. When prototype is undefined, Object.create() fails
+require('st' + 'ream').prototype = {};
