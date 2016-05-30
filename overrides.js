6cdcd11ced8854f2d9258af2f447777f9d740b37
Sander van der Burg <svanderburg@mgail.com>
2016-05-30 17:07:04 +0200

    Only apply stream workaround if the runtime provides one

--- overrides.js
+++ overrides.js
@@ -3,5 +3,14 @@
 // Fool the XMPP library to believe that it runs in a browser
 process.browser = true;
 
-// Set the prototype to an empty object of the result stream instance. When prototype is undefined, Object.create() fails
-require('st' + 'ream').prototype = {};
+// Set the prototype to an empty object of the result stream instance. When
+// prototype is undefined, Object.create() fails
+// This fix should only be provided if the runtime provides a 'stream' module,
+// which is the reason we wrap in into a try-catch block..
+
+try {
+    var stream = require('st' + 'ream');
+    stream.prototype = {};
+} catch(ex) {
+    // Just ignore if it didn't work
+}
