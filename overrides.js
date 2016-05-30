4659382668f4032d884e1dea9aca15a3fbeb1ecb
Sander van der Burg <svanderburg@mgail.com>
2016-05-30 17:17:20 +0200

    Fix typos

--- overrides.js
+++ overrides.js
@@ -3,10 +3,10 @@
 // Fool the XMPP library to believe that it runs in a browser
 process.browser = true;
 
-// Set the prototype to an empty object of the result stream instance. When
-// prototype is undefined, Object.create() fails
-// This fix should only be provided if the runtime provides a 'stream' module,
-// which is the reason we wrap in into a try-catch block..
+// Set the prototype to an empty object of the resulting stream instance. When
+// the prototype property of a constructor is undefined, Object.create() fails.
+// This fix should only be applied if the runtime provides a 'stream' module,
+// which is the reason why we wrap it into a try-catch block...
 
 try {
     var stream = require('st' + 'ream');
